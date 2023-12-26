c517c2ed83c20f86b3a28183da14b76e34799240
enansari <rahmat2022a@gmail.com>
2023-12-26 19:53:15 +0330

    update: update readme and r file

--- presentation/r/ischemia.R
+++ presentation/r/ischemia.R
@@ -1,3 +1,11 @@
+library(ggplot2)
+library(reshape2)
+
 setwd("C:/DATA/uni/bio/bioinformatics-hsu/presentation/r")
 isch <- read.delim("./isch.txt")
+isch <- na.omit(isch)
+isch <- subset(isch, a < 0.2)
 
+x <- isch[, 4:7]
+x.m <- melt(x)
+ggplot(x.m, aes(variable, value)) + geom_boxplot()
